f3f56d33400a671394b321a28bd42dfaa78d8d25
Hari Sekhon <harisekhon@gmail.com>
2025-02-14 16:28:29 +0700

    updated check_haproxy_syntax.sh

--- check_haproxy_syntax.sh
+++ check_haproxy_syntax.sh
@@ -42,7 +42,7 @@ test_haproxy_conf(){
     local cfg="$1"
     local str
     str="$(printf "%-${maxwidth}s " "$cfg:")"
-    if haproxy -v | grep -Eqi '^HA?Proxy version (1|2\.0)'; then
+    if haproxy -v | grep -Eqi '^HA?Proxy version (1|2\.[01])'; then
         if grep -q '^[[:space:]]*http-check send meth' "$cfg"; then
             echo "Skipping due to old HAProxy version not supporting 'http-check send meth'"
             return
